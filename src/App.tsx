e09423e566534a1a89012cc9408f54b090245dd1
zfx1509 <zfx1509@163.com>
2021-07-22 10:06:23 +0800

    初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import { createApp, defineComponent, h, reactive, ref } from 'vue'
+import { createApp, defineComponent, h, reactive, ref, Ref } from 'vue'
+import MonacoEditor from './components/MonacoEditor'
+import { createUseStyles } from 'vue-jss'
+
+function toJSON(data: any) {
+  return JSON.stringify(data, null, 2)
+}
+const schema = {
+  type: 'string',
+}
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
 
 export default defineComponent({
   setup() {
+    const schemaRef: Ref<any> = ref(schema)
+    const handleCodeChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (err) {
+        schemaRef.value = schema
+      }
+    }
+
+    const classesRef = useStyles()
     return () => {
-      return <div id="app">1</div>
+      const classes = classesRef.value
+      const code = toJSON(schemaRef.value)
+      return (
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handleCodeChange}
+            title="Schema"
+            class={classes.editor}
+          />
+        </div>
+      )
     }
   },
 })
